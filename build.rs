84c521897b3fd133903f340bd9cd75c6f6b7d970
Maou Shimazu <45318740+Maou-Shimazu@users.noreply.github.com>
2021-12-31 20:43:53 -0600

    fixed build config

--- build.rs
+++ build.rs
@@ -1,38 +1,10 @@
-fn cp(){
-    cc::Build::new()
-        .cpp(true)
-        .file("cp.cpp")
-        .cpp_link_stdlib("libstdc++")
-        .compile("cp")
-}
-
-fn cpfc(){
-    cc::Build::new()
-        .cpp(true)
-        .file("cpfc.cpp")
-        .cpp_link_stdlib("libstdc++")
-        .compile("cpfc")
-}
-
-fn cpfd(){
-    cc::Build::new()
-        .cpp(true)
-        .file("cpfd.cpp")
-        .cpp_link_stdlib("libstdc++")
-        .compile("cpfd")
-}
-
-fn cpfile(){
-    cc::Build::new()
-        .cpp(true)
-        .file("cpfile.cpp")
-        .cpp_link_stdlib("libstdc++")
-        .compile("cpfile")
-}
-
 fn main() {
-    cp();
-    cpfc();
-    cpfd();
-    cpfile();
+    cc::Build::new()
+    .cpp(true)
+    .file("cp.cpp")
+    .file("cpfc.cpp")
+    .file("cpfd.cpp")
+    .file("cpfile.cpp")
+    .cpp_link_stdlib("libstdc++")
+    .compile("cp")
 }
