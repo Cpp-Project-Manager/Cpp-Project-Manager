47afa082779db3bc78422c0420c52893ba87b09f
MaouShimazu <eldaddanladi@gmail.com>
2022-01-01 11:43:47 -0600

    added option for cpproject

--- src/cp.cpp
+++ src/cp.cpp
@@ -16,7 +16,8 @@ int main(){
 2) CP Folder/File List
 3) CP Folder Removal
 4) CP File Creation
-5) Exit
+5) CP Project Creation
+6) Exit
     )""""; // const char C multi line string, it can be changed to a non constant char if need be. Warnings may be generatied.
     std::cout << menu << std::endl;
     std::cin >> option; // input check. can be changed to only accept numbers and recursively run if a valid number is not given. 
@@ -35,6 +36,9 @@ int main(){
         system("cpfile");
         break;
         case 5:
+        system("cpproject");
+        break;
+        case 6:
         std::cout << "Thank you for using CP File." << std::endl;
         return 0;
         break; // no default case
