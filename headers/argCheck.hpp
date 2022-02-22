1c619c71634b405d0cf78ad642676037b6cf0cb9
Eldad Danladi <eldaddanladi@gmail.com>
2022-02-21 23:20:54 -0600

    ArgCheck Add

--- headers/argCheck.hpp
+++ headers/argCheck.hpp
@@ -10,12 +10,12 @@ namespace arguments {
  * @note   
  * @param  argc: int, takes main argc
  * @param  argv[]: int, takes main argv
- * @param  arguments: int, which argument it is at, 2> for 2 or more aruguments on program call
- * @param  argvParameter: int, index of argv, 1> for it to work
+ * @param  arguments: int, the amount of keywords that are going to be called after the program name
+ * @param  argvParameter: int, index of each word
  * @param  word: sting, word that should be compared with argv
  * @retval true: if arguments are compared false: otherwise
  */
-bool argCheck(int argc, char* argv[], int arguments, int argvParameter, const char* word){
+bool argCheck(const char* word = "", int argc = NULL, char* argv[] = NULL, int arguments = 2, int argvParameter = 1){
     if ( (argc == arguments) && _stricmp( argv[argvParameter], word) == 0)
         return true;
     else return false;
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 #include "../headers/projectManager.hpp"
 #include "../headers/conv.hpp"
 #include "../headers/argCheck.hpp"
+
+using namespace arguments;
+
 /*
  * CP
  *
@@ -13,21 +16,31 @@
  * If any new files are created, you want to add a new option and also add it to the switch.
  */
 int main(int argc, char *argv[]){
-    int option; // switch option
-    std::cout << "Welcome to Creation Project. What would you like to do?: " << std::endl;
-    while(true){
-        std::cout << cmdInformation::menu << std::endl;
-        std::cin >> option; // input check. can be changed to only accept numbers and recursively run if a valid number is not given. 
-        // todo: migrate all files here to main file
-        // todo: create something similar to cargo's `cargo new` function
-        switch(option){
-            // case 1: createFolder(); break;
-            // case 2: system("tree /f"); system("cp"); break;
-            // case 3: removeFolder(); break; 
-            // case 4: mainFunction(); break;
-            // case 5: cpproject(); break;
-            // case 6: std::cout << "Thank you for using CP File." << std::endl; return 0; break; // no default case
-        }
+    if(argCheck("Create", argc, argv)){
+        std::cout << "CP Create Called" << std::endl;
     }
+    if(argCheck("Projects", argc, argv)){
+        std::cout << "CP Projects Called" << std::endl;
+    }
+    if(argCheck("Add", argc, argv)){
+        std::cout << "CP Add Called" << std::endl;
+    }
+
+    // int option; // switch option
+    // std::cout << "Welcome to Creation Project. What would you like to do?: " << std::endl;
+    // while(true){
+    //     std::cout << cmdInformation::menu << std::endl;
+    //     std::cin >> option; // input check. can be changed to only accept numbers and recursively run if a valid number is not given. 
+    //     // todo: migrate all files here to main file
+    //     // todo: create something similar to cargo's `cargo new` function
+    //     switch(option){
+    //         // case 1: createFolder(); break;
+    //         // case 2: system("tree /f"); system("cp"); break;
+    //         // case 3: removeFolder(); break; 
+    //         // case 4: mainFunction(); break;
+    //         // case 5: cpproject(); break;
+    //         // case 6: std::cout << "Thank you for using CP File." << std::endl; return 0; break; // no default case
+    //     }
+    // }
     
 }
