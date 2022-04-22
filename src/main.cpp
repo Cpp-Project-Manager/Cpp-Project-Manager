bbb4d7ba3cf27da781e7b17562d7adca0ec32c59
Maou-Shimazu <eldaddanladi@gmail.com>
2022-04-22 04:14:57 -0600

    adding features

--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,47 @@
 #include <windows.h>
 #include <filesystem>
 
-#include <fmt/core.h>
-#include <fmt/color.h>
-#include <src/format.cc>
-#include <parse_args.h>
-
-#ifndef FMT_CORE_H_ // Use this and comment the one above out.
 #include "../fmt/include/fmt/core.h"
 #include "../fmt/include/fmt/color.h"
 #include "../fmt/src/format.cc"
-#endif
 
-#include "include/misc.hpp"
 #include "include/create.hpp"
 using namespace conv;
 
-//todo: impliment a faulty command reply
+// todo: impliment a faulty command reply
 
-int main(int argc, char *argv[]){
-    ParseArgs args = ParseArgs(argc, argv);
-    Create create;
+int main(int argc, char *argv[]) {
+  ParseArgs args = ParseArgs(argc, argv);
+  Cppm cppm;
 
-    if(args.DefaultParse("create"))
-        create.NewProject();
+  if (args.DefaultParse("create"))
+    cppm.NewProject();
 
-    if(args.DefaultParse("help"))
-        fmt::print(fg(fmt::color::sky_blue),"{}", cmdInformation::help);
+  if (args.DefaultParse("help"))
+    fmt::print(fg(fmt::color::sky_blue), "{}", cmdInformation::help);
 
-    if(args.Parse("new", 1)){
-        create.New(argv);
-    }
-    /*todo:
-    Open anywhere from terminal using toml++ and (if need be, create)dirs.
-    Impliment a Run, Build, Clean and Release using makefile.
-    Remove cppm create.
-    Crossplatform code.
-    */
+  if (args.Parse("new", 1)) {
+    cppm.New(argv);
+  }
+
+  if (args.Parse("run", 1)) {
+    // cppm.run();
+  }
+  if (args.Parse("build", 1)) {
+    // cppm.Build();
+  }
+
+  if (args.Parse("release", 1)) {
+    // cppm.Release();
+  }
+
+  if (args.Parse("clean", 1)) {
+    // cppm.Clean();
+  }
+  /*todo:
+  Open anywhere from terminal using toml++ and (if need be, create)dirs.
+  Impliment a Run, Build, Clean and Release using makefile.
+  Remove cppm create.
+  Crossplatform code.
+  */
 }
