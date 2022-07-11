edcc176d66f16863fb63fdadef6bfa5f788e5c2c
Maou-Shimazu <eldaddanladi@gmail.com>
2022-07-11 01:37:20 -0600

    documentation and metadata integration

--- src/cppm/builder.rs
+++ src/cppm/builder.rs
@@ -1,4 +1,6 @@
 use std::process::*;
+/// Compilers enum
+/// note: look into support for cppm and msvc
 pub enum Compilers {
     Gcc,
     Clang,
@@ -7,6 +9,18 @@ pub enum Compilers {
     Clangpp,
 }
 
+/// Returns a result of the processed params. Used to determine if a program exists or not. \
+/// usage:
+/// ```rs
+/// if crate::cppm::builder::subprocess("clang-format", "--version").is_err() {
+///     println!(
+///         "   {}",
+///         "Could not run clang-format. Please ensure that it is installed and on your PATH."
+///         .bright_red()
+///     );
+///     return;
+/// }
+/// ```
 pub fn subprocess(process: &str, arg: &str) -> std::io::Result<ExitStatus> {
     Command::new(process)
         .arg(arg)
@@ -15,6 +29,7 @@ pub fn subprocess(process: &str, arg: &str) -> std::io::Result<ExitStatus> {
         .status()
 }
 
+/// Returns either clang or gcc enum depending on existing one. Clang takes presedence.
 pub fn c() -> std::io::Result<Compilers> {
     if subprocess("clang", "-v").is_ok() {
         println!("Clang is installed.");
@@ -31,6 +46,7 @@ pub fn c() -> std::io::Result<Compilers> {
     }
 }
 
+/// Returns either clang++ or g++ enum depending on existing one. Clang++ takes presedence.
 pub fn cpp() -> std::io::Result<Compilers> {
     if subprocess("clang++", "-v").is_ok() {
         println!("Clang++ is installed.");
